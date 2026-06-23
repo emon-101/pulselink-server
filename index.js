d9a82a8eb23cc1dcd31e68e211204a8d1fe4d6a5
emon-101 <emon26.ugv@gmail.com>
2026-06-23 15:22:26 +0600

    feat: add DELETE API endpoint for removing donation requests

--- index.js
+++ index.js
@@ -65,6 +65,14 @@ async function run() {
         res.send(result);
     })
 
+    app.delete('/api/donation-request/:id', async(req, res) => {
+        const { id } = req.params;
+        const result = await donationsRequestCollection.deleteOne({
+            _id: new ObjectId(id)
+        });
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
